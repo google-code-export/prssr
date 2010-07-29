5fba0fb8886c2845084dbf17a31549f31ae3e176
mja-google@union06.de <mja-google@union06.de@bdfa3b71-6452-0410-9a48-a31a74f5c065>
2010-07-29 16:36:06 +0000

    Version info 1.4.5 for about dialog

--- share/version.h
+++ share/version.h
@@ -2,6 +2,6 @@
 
 #define VERSION_MAJOR						1
 #define VERSION_MINOR						4
-#define VERSION_PATCH						4
+#define VERSION_PATCH						5
 
-#define RC_VERSION_TEXT 					"pRSSreader 1.4.4"
+#define RC_VERSION_TEXT 					"pRSSreader 1.4.5"
